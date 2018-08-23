7a8564fd41ed055c8759ecbf509f960b375c169b
EHelander <eric.helander@brightpeakfinancial.com>
2018-08-23 17:22:15 -0500

    added additional projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [`
+  //   h3 {
+  //     color: dodgerblue;
+  //   }`]
 })
 export class AppComponent {
-  title = 'my-first-app';
 }
